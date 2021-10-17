6e80f57f7b16228d595cd5571062d797c0d69fe5
Sof7ik <alekse-bychkov@rambler.ru>
2021-10-18 01:00:25 +0300

    3d-graphic

---
+++ graphic-3d-2-laba-zashita/laba-2-zashita.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+float coordinateIsInSphere(float x, float y, float z)
+{
+    return pow(x, 2) + pow(y, 2) + pow(z, 2);
+}
+
+bool coordinateIsInCylinder(float x, float y, float z)
+{
+    return sqrt((pow(z - y, 2) + pow(x - z, 2) + pow(y - x, 2)) / 3) < 0.2;
+}
+
+int main()
+{
+    using namespace std;
+
+    setlocale(0, "Russian");
+
+    float x, y, z = 0;
+    float Radius = 1;
+
+    cout << "Введите Ваши точки..." << endl;
+    cout << "X = ";
+    cin >> x;
+    cout << "Y = ";
+    cin >> y;
+    cout << "Z = ";
+    cin >> z;
+
+    if (coordinateIsInCylinder(x, y, z))
+    {
+        cout << "Точка в цилиндре";
+        return 0;
+    }
+
+    if (coordinateIsInSphere(x, y, z) < pow(2, Radius) && !coordinateIsInCylinder(x, y, z))
+    {
+        cout << "Точка в сфере";
+    }
+    else if (coordinateIsInSphere(x, y, z) >= pow(2, Radius) && !coordinateIsInCylinder(x, y, z))
+    {
+        cout << "Точка вне сферы";
+    }
+}
